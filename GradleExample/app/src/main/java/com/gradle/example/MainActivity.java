9b5a74450b6452c23281bbf54138ab0b90235cdb
Chokkar G <chokkar.g@gmail.com>
2016-04-23 15:29:45 +0530

    Updated with Product flavours build gradle.

--- GradleExample/app/src/main/java/com/gradle/example/MainActivity.java
+++ GradleExample/app/src/main/java/com/gradle/example/MainActivity.java
@@ -3,21 +3,36 @@ package com.gradle.example;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(IConstants.freeType == IConstants.Type.FREE){
+            Log.i(TAG , "Free version");
+        } else{
+            Log.i(TAG , "Paid version");
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayUseLogoEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ GradleExample/app/src/paidFlavour/java/com.gradle.example/IConstants.java
@@ -0,0 +1,17 @@
+package com.gradle.example;
+
+/**
+ * Created by Chokkar on 4/23/2016.
+ */
+public class IConstants {
+
+    // Here I am gonna use Enum - So need to check hardcode string in class file
+
+
+    public enum Type {
+        FREE , PAID;
+    }
+
+    public static Type freeType = Type.PAID;
+
+}
